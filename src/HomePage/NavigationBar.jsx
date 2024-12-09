ccd825497ee1aa1d3136669c093604190b321d31
jeffzhang <xiaohuz7@uci.edu>
2024-12-08 21:16:57 -0800

    icon

--- src/HomePage/NavigationBar.jsx
+++ src/HomePage/NavigationBar.jsx
@@ -3,33 +3,33 @@ import styled from "styled-components";
 
 const navigationItems = [
     {
-        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/5a364d0093309bead42541a9d8cab4e8338cc672beff8e21bd7854b90dee1138?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502",
+        icon:"images/home.svg",
         label: "Home",
         active: true
     },
     {
-        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/bf6cf44042fada8d717241279884cc57760a53ffa4443dbcc83ebee6f2820985?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502",
+        icon: "images/Short.svg",
         label: "Shorts"
     },
     {
-        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/c14c05fe9c3ffef79d56386870e748d07563d4f8dada82b6d9807f289aa0db03?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502",
+        icon: "images/Subs.svg",
         label: "Subscriptions"
     },
     {
-        icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/9ec6bdb3f4320871cf54479175758299d2554b238898356ba57221c6032df8f1?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502",
+        icon: "images/music.svg",
         label: "YouTube Music"
     }
 ];
 
 const userItems = [
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/9ec6bdb3f4320871cf54479175758299d2554b238898356ba57221c6032df8f1?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "History" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/24af8b646eb845740dd9a556b1ac9f4dd6b6a17a8c6c210bc7818be451515179?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Playlists" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/ac9201b434fbb57f9ba1d9d63006f88ab26a3eef3ef6bc350fb19642fcf9d6a7?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Your videos" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/970283239a876bcf077c7566f0dbee2b3bd453adc3fee32057a9e56a036eb467?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Watch Later" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/34a75eda9691538a33db2499e0a959de7cfe5d6f7a4596ee417a1dbfa2c88c07?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Liked videos" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/86db2cd5b1cdd5083b2954973bc7f6e6c23cee7fcfccd85f6cb71a481113aef5?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Downloads" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/d672b6d725c97ee0bee0eded5c271c9f547bd31d73c0c2c04f54c8996b23234e?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Your clips" },
-    { icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/ba1d09369bfe7fad7bc2e93b5671afa7d8230ec57ef067e34c5f021f8706a460?placeholderIfAbsent=true&apiKey=484c4e2118754c79890e12514f9d5502", label: "Notebook" }
+    { icon: "images/history.svg", label: "History" },
+    { icon: "images/list.svg", label: "Playlists" },
+    { icon: "images/your.svg", label: "Your videos" },
+    { icon: "images/later.svg", label: "Watch Later" },
+    { icon: "images/like.svg", label: "Liked videos" },
+    { icon: "images/down.svg", label: "Downloads" },
+    { icon: "images/clip.svg", label: "Your clips" },
+    { icon: "images/note.svg", label: "Notebook" }
 ];
 
 const subscriptions = [
